943c3c5052170756803047c9823000f61acbcad1
sangyeol-kim <twysg@likelion.org>
2019-04-16 21:47:01 +0900

    Update 2750.py

--- boj/2750.py
+++ boj/2750.py
@@ -1,34 +1,8 @@
-
-# strList = []
-# while True:
-#     try:
-#         strList.append(int(input('Enter the number > ')))
-#     except ValueError:
-#         print('Invalid number. Please try again!')
-#         continue
-# print(strList)
-
-
-# def range(list):
-#     print(list)
-#     range_list = []
-#     for num in range(0, len(list)):
-#         if not range_list:
-#             range_list.append(num)
-#         elif num not in range_list:
-#             range_list.append(num)
-#         else:
-#             break
-#     return range_list
-
-
-# print(range(strList))
-
-number = int(input("몇개?"))
+number = int(input())
 arr = []
 
 for num in range(0, number):
-    num = int(input(">"))
+    num = int(input())
     arr.append(num)
     arr.sort()
 
