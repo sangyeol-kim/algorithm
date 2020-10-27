30aca3c803f4fed29de745a44876ddca069c416a
김상열 <twysg@likelion.org>
2020-10-27 23:32:33 +0900

    Feat: 백준 2798번 코드

---
+++ boj/2798.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+const input = fs
+  .readFileSync("/dev/stdin", "utf8")
+  .trim()
+  .split('\n');
+
+const cardCount = parseInt(input[0].split(' ')[0], 10);
+const targetNumber = parseInt(input[0].split(' ')[1], 10);
+const sortedSpreadCard = input[1].split(' ').sort().map((cardNumber) => parseInt(cardNumber, 10));
+
+let result = 0;
+
+for (let i = 0; i < cardCount; i++) {
+  for (let j = 1; j < cardCount; j++) {
+    for (let k = 2; k < cardCount; k++) {
+      const cardNumbers = [sortedSpreadCard[i], sortedSpreadCard[j], sortedSpreadCard[k]];
+
+      if ([...new Set(cardNumbers)].length === 3) {
+        const totalCard = sortedSpreadCard[i] + sortedSpreadCard[j] + sortedSpreadCard[k];
+
+        if (totalCard <= targetNumber && totalCard > result) {
+          result = totalCard;
+        }
+      }
+    }
+  }
+}
